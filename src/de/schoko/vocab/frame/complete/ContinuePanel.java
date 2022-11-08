0b7cd10a97990c9f49b4e4fbe782cc2c07cda7e9
WeiseSchokolade <weiseschokolade@weiseschokolade.github.io>
2022-11-08 18:50:00 +0100

    Changed order of buttons in Asking Complete menu

--- src/de/schoko/vocab/frame/complete/ContinuePanel.java
+++ src/de/schoko/vocab/frame/complete/ContinuePanel.java
@@ -13,18 +13,19 @@ public class ContinuePanel extends JPanel {
 	private static final long serialVersionUID = 1L;
 	
 	public ContinuePanel() {
-		JButton continueButton = new JButton(Strings.BUTTON_CONTINUE);
-		continueButton.addActionListener((event) -> {
-			GenericDataHolder.mainMenu();
-		});
-		continueButton.setPreferredSize(new Dimension(150, 30));
-		this.add(continueButton, BorderLayout.EAST);
 		JButton retryButton = new JButton(Strings.BUTTON_RETRY);
 		retryButton.addActionListener((event) -> {
 			GenericDataHolder.start(GenericDataHolder.getVocab().copy());
 		});
-		retryButton.setPreferredSize(new Dimension(150, 30));
+		retryButton.setPreferredSize(new Dimension(250, 30));
 		this.add(retryButton, BorderLayout.WEST);
+		
+		JButton continueButton = new JButton(Strings.BUTTON_CONTINUE);
+		continueButton.addActionListener((event) -> {
+			GenericDataHolder.mainMenu();
+		});
+		continueButton.setPreferredSize(new Dimension(250, 30));
+		this.add(continueButton, BorderLayout.EAST);
 		continueButton.requestFocusInWindow();
 	}
 }
