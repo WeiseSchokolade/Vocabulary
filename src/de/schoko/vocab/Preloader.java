64e404216b77d35193312b4088083ba57a6d236e
WeiseSchokolade <weiseschokolade@weiseschokolade.github.io>
2022-11-09 15:46:55 +0100

    Added support for German and English.
    The languages are taken from the Language of the JVM and not yet selectable
    
    Signed-off-by: WeiseSchokolade <weiseschokolade@weiseschokolade.github.io>

--- src/de/schoko/vocab/Preloader.java
+++ src/de/schoko/vocab/Preloader.java
@@ -7,11 +7,13 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 import de.schoko.vocab.exceptions.LoadException;
 import de.schoko.vocab.resources.InternalResourceList;
 import de.schoko.vocab.resources.StringLoader;
+import de.schoko.utility.ArrayUtility;
 import de.schoko.utility.Empty;
 import de.schoko.utility.Logging;
 
@@ -19,6 +21,13 @@ public class Preloader {
 	private String jarLocation = "";
 	private String workspaceLocation = "";
 	private String vocabLocation = "";
+	/**
+	 * Language code from ISO 639-1
+	 * <br>
+	 * <a href="https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes">List Of ISO 639-1 Codes On Wikipedia</a>
+	 */
+	private String languageCode = "";
+	private String languageLocation = "";
 	
 	private VocabLoader vocabLoader;
 	
@@ -46,11 +55,20 @@ public class Preloader {
 		Logging.logInfo("Workspace Location: " + workspaceLocation);
 		this.vocabLoader = new VocabLoader();
 		
+		languageCode = Locale.getDefault().getLanguage();
+		if (!ArrayUtility.contains(InternalResourceList.SUPPORTED_LANGUAGES, languageCode)) {
+			Logging.logWarning("Couldn't find " + languageCode + " in supported languages. Using default (english en).");
+			languageCode = Locale.ENGLISH.getLanguage();
+		}
+		languageLocation = InternalResourceList.LANGUAGES_LOCATION + languageCode + InternalResourceList.RESOURCE_PATH_SEPERATOR;
+		Logging.logInfo("Language location: " + languageLocation);
+		
 		Map<String, String> translations = new HashMap<>();
 		try {
-			InputStream in = getClass().getResourceAsStream(InternalResourceList.TRANSLATION_LOCATION);
+			InputStream in = getClass().getResourceAsStream(languageLocation + "Strings.txt");
 			BufferedReader fileReader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
 			
+			int translationAmount = 0;
 			while (true) {
 				String line = fileReader.readLine();
 				if (line == null) {
@@ -59,6 +77,7 @@ public class Preloader {
 				String[] pairs = line.split(" = ");
 				if (pairs.length == 2) {
 					translations.put(pairs[0], pairs[1]);
+					translationAmount++;
 				} else {
 					continue;
 				}
@@ -66,9 +85,11 @@ public class Preloader {
 			
 			in.close();
 			fileReader.close();
+			Logging.logInfo("Successfully loaded " + translationAmount + " translation(s)");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
 		StringLoader stringLoader = new StringLoader();
 		stringLoader.load(translations);
 		
--- src/de/schoko/vocab/resources/InternalResourceList.java
+++ src/de/schoko/vocab/resources/InternalResourceList.java
@@ -3,5 +3,9 @@ package de.schoko.vocab.resources;
 public class InternalResourceList {
 	public static final String ICON_LOCATION = "de/schoko/vocab/resources/icon.png",
 			TRANSLATION_LOCATION = "/de/schoko/vocab/resources/translations.txt",
-			STYLEGUIDE_LOCATION = "/de/schoko/vocab/resources/styleguide.txt";
+			LANGUAGES_LOCATION = "/de/schoko/vocab/resources/",
+			STYLEGUIDE_LOCATION = "/de/schoko/vocab/resources/styleguide.txt",
+			RESOURCE_PATH_SEPERATOR = "/";
+	
+	public static final String[] SUPPORTED_LANGUAGES = {"de", "en"};
 }
